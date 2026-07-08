8aa282277072780ea3d621bfcefe2f453bc0ae04
FlavioT04 <139927186+FlavioT04@users.noreply.github.com>
2026-07-08 13:21:41 -0230

    components wired up and style added to website

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import Projects from './components/Projects'
 import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Certificates from './components/Certificates'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <About />
-      <Projects />
-      <Skills />
-      <Experience />
-      <Certificates />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Skills />
+        <Experience />
+        <Certificates />
+        <Contact />
+      </main>
+      <Footer />
     </>
   )
 }
