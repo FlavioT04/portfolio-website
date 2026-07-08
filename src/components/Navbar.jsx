f10fb8cb866185c5be573ad3adb303effc5ffe57
FlavioT04 <139927186+FlavioT04@users.noreply.github.com>
2026-07-08 13:16:30 -0230

    added classNames to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,20 @@
-import { portfolio } from "../data"
+import { portfolio } from '../data'
 
 function Navbar() {
-    return (
-        <nav>
-            <span>{portfolio.shortName}</span>
-            <ul>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#skills">Skills</a></li>
-                <li><a href="#Experience">Contact</a></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <span className="navbar-name">{portfolio.shortName}</span>
+        <ul className="navbar-links">
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#experience">Experience</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
